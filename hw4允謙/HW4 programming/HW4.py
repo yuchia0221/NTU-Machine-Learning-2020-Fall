1419bc2d63f1ec79ac530ade6c87bb3e1b34fecf
Yun Chien Lo <curtis0226@gmail.com>
2020-12-04 16:44:43 +0800

    hw4 允謙

---
+++ hw4允謙/HW4 programming/HW4.py
@@ -0,0 +1,8 @@
+from liblinearutil import *
+import pandas as pd
+import numpy as np
+
+train_data = pd.read_table("hw4_train.dat.txt", header=None, names=["x_{1}", "x_{2}", "x_{3}", "x_{4}", "x_{5}", "x_{6}", "y"], sep=" ")
+
+help(train)
+# phi transform
